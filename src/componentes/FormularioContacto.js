5e9779e1af26aa27e6eecd8e1f0d7131e44bd670
Jruizdev <jonathan.ruizolvera@gmail.com>
2024-03-29 17:58:47 -0600

    actualizacion

--- src/componentes/FormularioContacto.js
+++ src/componentes/FormularioContacto.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { ModalEmailEnviado } from './ModalEmailEnviado';
 import { Overlay, Popover } from 'react-bootstrap';
 
