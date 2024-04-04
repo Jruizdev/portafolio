a07bf7ccaae6182dc05666c13a451e02f9bc06d8
Jruizdev <jonathan.ruizolvera@gmail.com>
2024-04-03 21:28:51 -0600

    Actualización

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App () {
               <h3>Hola, soy</h3>
               <h1><strong>Jonathan Ruiz Olvera</strong></h1>
               <h3 className="texto-resaltar">Desarrollador de Software</h3>
-              <div className="row w-100 text-center text-md-start">
+              <div className="row w-100 text-center text-md-start m-auto">
                 <div className="col-12">
                   <a href='http://jrodev.x10.mx/recursos/CV%20%Jonathan%20%Ruiz.pdf' target='_blank' download="CV_Jonathan_Ruiz.pdf">
                     <button className="btn btn-primary me-3 px-3 rounded-pill">Descargar CV</button>
